ae558f8f3de7dc2cc61a41fa634e206d6cc727a9
Philippe Leduc <philippe.leduc@wandercraft.eu>
2020-04-20 14:42:58 +0200

    Add an ewample that let the compiler handle auto gracefully through an anonymous template

--- BoostSML/test/test_fsm_in_class.cpp
+++ BoostSML/test/test_fsm_in_class.cpp
@@ -2,21 +2,41 @@
 
 #include "sml.hpp"
 
-#include <fstream>
+#include <iostream>
 #include <memory>
 #include "sml_logging.hpp"
 
 namespace sml = boost::sml;
 using namespace sml;
 
-namespace {
-    
-class Truc
+
+template <class R, class... Ts>
+auto call_impl(R (*f)(Ts...)) {
+  return [f](Ts... args) { return f(args...); };
+}
+template <class T, class R, class... Ts>
+auto call_impl(T* self, R (T::*f)(Ts...)) {
+  return [self, f](Ts... args) { return (self->*f)(args...); };
+}
+template <class T, class R, class... Ts>
+auto call_impl(const T* self, R (T::*f)(Ts...) const) {
+  return [self, f](Ts... args) { return (self->*f)(args...); };
+}
+template <class T, class R, class... Ts>
+auto call_impl(const T* self, R (T::*f)(Ts...)) {
+  return [self, f](Ts... args) { return (self->*f)(args...); };
+}
+
+auto call = [](auto... args) { return call_impl(args...); };
+
+
+template<typename>
+class BiduleT
 {
 public:
-    Truc();
+    BiduleT() = default;
+    ~BiduleT() = default;
     
-    ~Truc() = default;
     struct startController{};
     
     struct StartUpFSM {
@@ -24,6 +44,36 @@ public:
             using namespace sml;
             return make_transition_table(
                 "CalibrationCheck"_s    <= *"Ready"_s           + event<startController>
+            );
+        }
+    };
+    
+    my_logger logger;
+    using fsm_t = sml::sm<StartUpFSM, sml::logger<my_logger>>;
+    fsm_t sm_{logger};
+};
+using Bidule = BiduleT<int>;
+    
+
+void action() {std::cout <<"action" <<std::endl;}
+    
+    
+class Truc
+{
+public:
+    Truc();
+    ~Truc() = default;
+    
+    struct startController{};
+    
+    // Cannot call a non satic method !! 
+    static void action2() {std::cout <<"action2" <<std::endl;}
+    
+    struct StartUpFSM {
+        auto operator()() noexcept {
+            using namespace sml;
+            return make_transition_table(
+                "CalibrationCheck"_s    <= *"Ready"_s           + event<startController> / (call(action), call(Truc::action2))
             );
         }
     };
@@ -42,7 +92,9 @@ TEST(SML_FSMInClass, test)
 {
     Truc truc;
     truc.sm_->process_event(Truc::startController());
+    
+    Bidule bidule;
+    bidule.sm_.process_event(Bidule::startController());
 }
 
-}  // namespace
 
