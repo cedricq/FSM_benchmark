3e7c3ed0f15d8c8e0640381b2165fdcec0f9b472
Cedric Quentin <cedric.quentin@wandercraft.eu>
2020-10-14 16:30:23 +0200

    adding new more complex example

---
+++ MyFSM/readme.md
@@ -0,0 +1,159 @@
+# State Machine Design
+
+The new state machine needs to fix a couple of issues that we are already facing:
+
+- Having one state machine with all the states definition and transition is too hard to maintain as the number of state keeps increasing. When changing it the risk of regression is high. To solve that we need to have a state machine able to handle hierarchy. This way any state machine can be broken down into independent sub state machine.
+
+  <img src="Hierarchical-finite-state-machine-representation-of-the-scavenger-agent-Self-transitions.png" alt="Hierarchical-finite-state-machine" style="zoom:67%;" />
+
+```
+
+```
+
+- The possibility to create the state machine at runtime and to have possibly multi instances of them. For instance, if we had a state machine in the motor class then we need to be able to have 12 different instances of that same state machine.
+
+- The need to have a readable state machine "all-in-one-spot" definition
+
+  ### Design Description
+
+  The state machine design is only one header file, so far. It does contain 2 classes:
+
+  ```c++
+  class State_
+  {
+  public:
+      State_(std::string name);
+      virtual ~State_();
+  
+      void addTransition(Transition t);
+      
+      void setRun(std::function<void()> f);
+      void setOnEntry(std::function<void()> f);
+      void setOnExit(std::function<void()> f);
+      
+      virtual std::shared_ptr<State_> handleEvent(EventId const& e);
+      
+      std::function<void()> Run_     = NoAction;
+      std::function<void()> OnEntry_ = NoAction;
+      std::function<void()> OnExit_  = NoAction;
+  
+  private:
+      std::vector<Transition> transitionTable_ {};
+      std::string name_;
+  };
+  ```
+
+  `State_` is the default mother class definition of all states.
+
+  It does contain a vector of `Transition`which represents all the possible transitions it can switch to from this state.
+
+  A transition happens on an event, can trigger an action, can be conditional (guards) and knows the state it needs to switch to. This is the definition of the struct:
+
+  ```c++
+  struct Transition
+  {
+      EventId eventId;
+      std::shared_ptr<State_> stateTo;
+      std::function<bool_t()> guard;
+      std::function<void()> action;
+  };
+  ```
+
+  An event is just represented by an int id and a text string which is only needed for log purpose.
+
+  ```c++
+  struct EventId
+  {
+      int id;
+      std::string name;
+  };
+  ```
+
+  The second class is `StateManagerClass_`which actually handles the state transitions and which knows the current state we're in. It is a `State_`as well which enables state machines encapsulation.
+
+  ```c++
+  class StateManagerClass_ : public State_
+  {
+  public:
+      StateManagerClass_(std::string name);
+      virtual ~StateManagerClass_();
+  
+      virtual std::shared_ptr<State_> handleEvent(EventId const& e) override;
+      
+      void start();
+      void run();
+      void reset();
+      void setInitState(State state);
+      bool isState(std::string const& str);
+  
+  private:
+      State initState_ {nullptr};
+      State currentState_ {nullptr};
+  
+  };
+  ```
+
+  Know that the owner will need to manipulate shared pointers, thus to help the writing of the code these 2 helpers have been exposed:
+
+  ```c++
+  using State = std::shared_ptr<State_>;
+  using StateManager = std::shared_ptr<StateManagerClass_>;
+  ```
+
+  ### Simple example
+
+  This example is the exact same one used in the State Machine unit test:
+
+  ![Simple Example](simple_example.png)
+
+  
+
+  As code, once events and states have been declared, this state machine is defined as followed:
+
+  ```c++
+      State stateA = std::make_shared<StateA>();
+      State stateB = std::make_shared<StateB>();
+      StateManager mainState = std::make_shared<StateManagerClass_>("Main");
+      SET_INIT_STATE(mainState, stateA);
+  
+      State stateC = std::make_shared<StateC>();
+      StateManager subState = std::make_shared<StateManagerClass_>("Sub");
+      SET_INIT_STATE(subState, stateC);
+  
+      // MainState transitions
+      ADD_TRANSITION (stateA,    StartEvent,  stateB);
+      ADD_TRANSITION (stateA,    SubEvent,    subState);
+      ADD_TRANSITION_GUARD (stateA,
+                              GuardEvent,
+                              [] {return flag_guard;},
+                              stateB);
+  
+      ADD_TRANSITION_GUARD_ACTION (stateA,
+                                   ActionEvent,
+                                   [] {return true;},
+                                   [] {flag_delegated = true;},
+                                   stateB);
+  
+      ADD_TRANSITION (stateB,    FinishEvent, stateA);
+  
+      // Substate transitions
+      ADD_TRANSITION (subState,  FinishEvent, stateA);
+  
+      mainState->start();
+  ```
+
+  This is still an early design and will likely continue to evolve, however it enables already the main needed  features  (state machines encapsulation and creation at runtime).
+  
+  The state machine is typically called from a periodic routine by the following code example: 
+
+  ```c++
+  	...
+    mainState_->run();
+    if ( nextEvent_ != noEvent )
+    {
+        mainState_->handleEvent(nextEvent_);
+        nextEvent_ = noEvent;
+    }
+    ...
+  ```
+
--- MyFSM/test/test_myfsm.cpp
+++ MyFSM/test/test_myfsm.cpp
@@ -6,43 +6,43 @@
 
 using namespace core;
 
-EventId NoEvent{0, "NoEvent"};
-EventId StartEvent{1, "StartEvent"};
-EventId FinishEvent{2, "FinishEvent"};
-EventId SubEvent{3, "SubEvent"};
-EventId GuardEvent{4, "GuardEvent"};
-EventId ActionEvent{5, "ActionEvent"};
-
-class StateA : public State_ 
-{
- public:
-    StateA() : State_("A") {};
-    virtual ~StateA() {};
-};
-
-class StateB : public State_ 
-{
- public:
-    StateB() : State_("B") {};
-    virtual ~StateB() {};
-};
-
-class StateC : public State_ 
-{
- public:
-    StateC() : State_("C") {};
-    virtual ~StateC() {};
-};
-
-
 void Tick(StateManager& sm, EventId& event)
 {    
+    std::cout<<"Posting Event : " <<event.name.c_str() <<std::endl;
     sm->run();
     sm->handleEvent(event) ;
 }
 
 TEST(MyFSM, test)
 {
+    EventId NoEvent{0, "NoEvent"};
+    EventId StartEvent{1, "StartEvent"};
+    EventId FinishEvent{2, "FinishEvent"};
+    EventId SubEvent{3, "SubEvent"};
+    EventId GuardEvent{4, "GuardEvent"};
+    EventId ActionEvent{5, "ActionEvent"};
+    
+    class StateA : public State_ 
+    {
+    public:
+        StateA() : State_("A") {};
+        virtual ~StateA() {};
+    };
+
+    class StateB : public State_ 
+    {
+    public:
+        StateB() : State_("B") {};
+        virtual ~StateB() {};
+    };
+
+    class StateC : public State_ 
+    {
+    public:
+        StateC() : State_("C") {};
+        virtual ~StateC() {};
+    };
+    
     // Listing states
     State stateA = std::make_shared<StateA>();
     State stateB = std::make_shared<StateB>();
@@ -97,4 +97,98 @@ TEST(MyFSM, test)
     ASSERT_TRUE(mainState->isState("B"));
 }
 
+EventId NoEvent         {0, "NoEvent"};
+EventId AlertEvent      {1, "AlertEvent"};
+EventId CriticalEvent   {3, "CriticalEvent"};
+EventId EmergencyEvent  {4, "EmergencyEvent"};
+EventId FinishEvent     {5, "FinishEvent"};
+EventId ExerciseEvent   {6, "ExerciseEvent"};
+EventId TurnEvent       {7, "TurnEvent"};
+EventId WalkEvent       {8, "WalkEvent"};
+EventId RepositionEvent {9, "RepositionEvent"};
 
+StateManager supraState = std::make_shared<StateManagerClass_>("Supra");
+StateManager mainState  = std::make_shared<StateManagerClass_>("Main");
+
+void createSupraStateMachine()
+{
+    State Startup           = std::make_shared<State_>("Startup");
+    State GentleTrap        = std::make_shared<State_>("GentleTrap");
+    State Trap              = std::make_shared<State_>("Trap");
+    State Emergency         = std::make_shared<State_>("Emergency");
+    
+    
+    SET_INIT_STATE(supraState, Startup);
+    
+    ADD_TRANSITION (Startup,        FinishEvent,        mainState);
+    
+    ADD_TRANSITION (mainState,      AlertEvent,         GentleTrap);
+    ADD_TRANSITION (mainState,      CriticalEvent,      Trap);
+    ADD_TRANSITION (mainState,      EmergencyEvent,     Emergency);
+    
+    ADD_TRANSITION (GentleTrap,     AlertEvent,         Trap);
+    ADD_TRANSITION (GentleTrap,     CriticalEvent,      Trap);
+    ADD_TRANSITION (GentleTrap,     EmergencyEvent,     Emergency);
+    
+    ADD_TRANSITION (Trap,           CriticalEvent,      Emergency);
+    ADD_TRANSITION (Trap,           EmergencyEvent,     Emergency);
+}
+
+
+void createMainStateMachine()
+{
+    State Sitting           = std::make_shared<State_>("Sitting");
+    State Standing          = std::make_shared<State_>("Standing");
+    State Exercising        = std::make_shared<State_>("Exercising");
+    State Turning           = std::make_shared<State_>("Turning");
+    State Walking           = std::make_shared<State_>("Walking");
+    State Repositioning     = std::make_shared<State_>("Repositioning");
+    
+    SET_INIT_STATE(mainState, Sitting);
+    
+    ADD_TRANSITION (Sitting,        FinishEvent,        Standing);
+    
+    ADD_TRANSITION (Standing,       ExerciseEvent,      Exercising);
+    ADD_TRANSITION (Standing,       TurnEvent,          Turning);
+    ADD_TRANSITION (Standing,       WalkEvent,          Walking);
+    ADD_TRANSITION (Standing,       RepositionEvent,    Repositioning);
+    
+    ADD_TRANSITION (Exercising,     FinishEvent,        Standing);
+    ADD_TRANSITION (Turning,        FinishEvent,        Standing);
+    ADD_TRANSITION (Walking,        FinishEvent,        Standing);
+    ADD_TRANSITION (Repositioning,  FinishEvent,        Standing);
+    
+}
+
+TEST(MyFSM, CompleteFSM)
+{
+    createSupraStateMachine();
+    createMainStateMachine();
+    
+    ASSERT_TRUE(supraState->isState("Startup"));
+    Tick(supraState, FinishEvent);
+    ASSERT_TRUE(supraState->isState("Sitting"));
+    Tick(supraState, FinishEvent);
+    ASSERT_TRUE(supraState->isState("Standing"));
+    Tick(supraState, ExerciseEvent);
+    ASSERT_TRUE(supraState->isState("Exercising"));
+    Tick(supraState, FinishEvent);
+    ASSERT_TRUE(supraState->isState("Standing"));
+    Tick(supraState, WalkEvent);
+    ASSERT_TRUE(supraState->isState("Walking"));
+    Tick(supraState, FinishEvent);
+    ASSERT_TRUE(supraState->isState("Standing"));
+    Tick(supraState, TurnEvent);
+    ASSERT_TRUE(supraState->isState("Turning"));
+    Tick(supraState, FinishEvent);
+    ASSERT_TRUE(supraState->isState("Standing"));
+    Tick(supraState, RepositionEvent);
+    ASSERT_TRUE(supraState->isState("Repositioning"));
+    Tick(supraState, AlertEvent);
+    ASSERT_TRUE(supraState->isState("GentleTrap"));
+    Tick(supraState, AlertEvent);
+    ASSERT_TRUE(supraState->isState("Trap"));
+    Tick(supraState, CriticalEvent);
+    ASSERT_TRUE(supraState->isState("Emergency"));
+    
+}
